1b4de4c70b8c32a534095ce3c5ef9e5acc0cad31
joannamarieintia <joannaintia27@gmail.com>
2024-04-27 23:19:28 +0800

    add object type profile

---
+++ sound_sight/src/model/user_music_profile.ts
@@ -0,0 +1,9 @@
+export type Profile = {
+    username: string,
+    birthday: string,
+    age: number,
+    location: string, 
+    music_taste: string,
+    mood: string,
+    favorite_song: string
+}
