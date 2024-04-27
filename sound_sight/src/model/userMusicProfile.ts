0c57f5d23721b5d122007680262b69844436aaa4
joannamarieintia <joannaintia27@gmail.com>
2024-04-27 22:12:02 +0800

    create object type

---
+++ sound_sight/src/model/userMusicProfile.ts
@@ -0,0 +1,9 @@
+export interface Profile {
+    username: string,
+    birthday: string,
+    age: number,
+    location: string, 
+    music_taste: string,
+    mood: string
+    recent_fav_song: string
+}
