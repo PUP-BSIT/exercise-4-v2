614ae2ac45bb6ef7d5ff89c96e36331a406a3032
Andrei James Alejandro <squishycaptain09@gmail.com>
2024-04-29 23:16:32 +0800

    add Form for the Profile Input and Output

--- sound_sight/src/app/app.module.ts
+++ sound_sight/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './parent/child/child.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ChildComponent } from './parent/child/child.component';
     ChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
