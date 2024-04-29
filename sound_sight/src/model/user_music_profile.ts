e9c9e04a42c5de25f9033bdf934631738bbf9b14
Joanna Intia <146611304+intia-joannamarie@users.noreply.github.com>
2024-04-29 22:01:12 +0800

    Delete sound_sight directory

--- sound_sight/src/model/user_music_profile.ts
+++
@@ -1,9 +0,0 @@
-export type Profile = {
-    username: string,
-    birthday: string,
-    age: number,
-    location: string, 
-    music_taste: string,
-    mood: string,
-    favorite_song: string
-}
