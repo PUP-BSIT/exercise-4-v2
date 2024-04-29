90996604d88dbc14a2130878351a5bbe428091cc
despe_reymondchristian <chan.despe@gmail.com>
2024-04-29 22:35:37 +0800

    add changes in parent and child component

--- sound_sight/src/app/parent/child/child.component.ts
+++ sound_sight/src/app/parent/child/child.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../../model/user_music_profile';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
-
+  @Input() profile: Profile;
 }
--- sound_sight/src/app/parent/parent.component.ts
+++ sound_sight/src/app/parent/parent.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Profile } from '../../model/user_music_profile';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
-
+  profiles: Profile[] = []
 }
